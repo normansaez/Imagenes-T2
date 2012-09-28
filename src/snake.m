62e0cf97d9e3ac1e9826e76480091e1fc67332b3
Norman Saez <nsaez@alma.cl>
2012-09-28 00:06:50 -0300

    Fx Fy

--- src/snake.m
+++ src/snake.m
@@ -24,29 +24,33 @@ U = [u^2, u, 1];
 
 U_times_B =  U * B;
 
-u2eval = 20;
+u2eval = 10000;
 u = 0:1/u2eval:1-1/u2eval;
 
 C_s = struct([]);
-for j=2:1:length(x)-1
-    
-    px(1) = x(j-1);
-    px(2) = x(j);
-    px(3) = x(j+1);
-    
-    py(1) = y(j-1);
-    py(2) = y(j);
-    py(3) = y(j+1);
+for j=2:1:length(x)
+        px(1) = x(j-1);
+        px(2) = x(j);
+        
+        py(1) = y(j-1);
+        py(2) = y(j);
+        
+    if j == length(x)
+        px(3) = x(1);
+        py(3) = y(1);
+    else
+        px(3) = x(j+1);
+        py(3) = y(j+1);
+    end
     
     Sx =  U_times_B * px';
-    Sy =  U_times_B * py';    
+    Sy =  U_times_B * py';
     
     i = j - 1;
-    fprintf('seg %d\n',i)
+    fprintf('Seg %d\n',i)
     C_s(i).x = eval(Sx);
     C_s(i).y = eval(Sy);
-    plot(C_s(i).x,C_s(i).y)
-    hold on
 end
-   
+
+[Fx, Fy] = gradient(gradient(double(img)).^2);
 m = img;
